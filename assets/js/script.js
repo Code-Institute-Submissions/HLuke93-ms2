ce4a76ed5aa99354e2e61888c755599738ac6f50
HLuke93 <lukehickson2020@gmail.com>
2021-10-25 12:02:07 +0000

    add data choice to buttons in html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+/**Declaring all DOM elements */
+
+const userScore = document.getElementById("user-score");
+const userScore = document.getElementById("computer-score");
+const userMessage = document.getElementById("user-message");
+const computerMessage = document.getElementById("comp-message");
+const resultMessage = document.getElementById("result");
+
+const buttons = document.querySelectorAll("#btn")
